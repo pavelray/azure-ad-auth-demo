7144de3a153de781371344b0b29b6fafaadf2f13
Pavel Ray <35458270+pavelray@users.noreply.github.com>
2019-11-13 11:49:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Auth from './Auth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  displayUserInformation() {
+    const auth = new Auth();
+    if (auth.isLoggedIn()) {
+      console.log(auth.currentUser());
+      return (
+        <div>
+          <p>
+            Welcome, {auth.currentUser().name} from {auth.currentUser().city}, {auth.currentUser().country}
+          </p>
+          <a 
+            className="App-link" 
+            href="/abc" onClick={() => auth.logout()}
+          >
+            Sign Out
+          </a> 
+        </div>
+      );
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.displayUserInformation()}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import b2cauth from 'react-azure-adb2c';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+b2cauth.initialize({
+    instance: 'https://login.microsoftonline.com/tfp/', 
+    tenant: 'schooltoolb2c.onmicrosoft.com',
+    signInPolicy: 'B2C_1_schooltool_signup',
+    applicationId: '29907098-0e87-4652-9484-8d90c92c8742',
+    cacheLocation: 'sessionStorage',
+    scopes: ['https://schooltoolb2c.onmicrosoft.com/schooltool/user_impersonation'],
+    redirectUri: 'http://localhost:3000',
+    postLogoutRedirectUri: window.location.origin,
+  });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+b2cauth.run(() => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+    serviceWorker.unregister();
+});
+
